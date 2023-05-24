3ef5b28df4cb7fc0e6fe50f6d136a430bcebc4f5
jwted <jgjoaogabriel@hotmail.com>
2023-05-24 18:02:21 +0100

    Professor João mandou fazer isto

--- Server/routes/occurrences.router.js
+++ Server/routes/occurrences.router.js
@@ -4,7 +4,7 @@ const {auth_user,auth_admin} = require('../controllers/auth.controller')
 const {getOccurrences,addOccurrence,editOccurrence,deleteOccurrence} = require('../controllers/occurrences.controller')
 
 router.route('/')
-    .get(auth_admin,getOccurrences)
+    .get(getOccurrences)
     .post(auth_user,addOccurrence)        
     
 
