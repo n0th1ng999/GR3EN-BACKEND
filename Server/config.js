aac99a8ce8db7debdaede6168c5cd07078fd45c2
n0th1ng999 <tiagogabrielpereira@gmail.com>
2023-06-05 19:54:29 +0100

    Cyclic 2

---
+++ Server/config.js
@@ -0,0 +1,26 @@
+    const config = {
+        
+        //DB URL
+        //tiagogabrielpereira
+        //password JmwFU84ZIs8AWgaS
+        databaseURL: process.env.MONGODB_URI,
+
+        //LOCALHOST 
+        hostname : '127.0.0.1',
+        
+        //PORTA  
+        port : process.env.PORT || 3000,
+
+        dbName:  process.env.Database,
+
+        //JWT stuff 
+        //SECRET
+        jwtSecret: process.env.jwtSecret,
+        //Max Age for the token in seconds
+        jwtMaxAge: 60 * 60 * 24 * 7 // Sete dias
+
+    }   
+
+
+    
+module.exports = config
