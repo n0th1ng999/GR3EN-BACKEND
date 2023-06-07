ef48dc550fd5a58437f27d097a52bd045f8f71b6
n0th1ng999 <tiagogabrielpereira@gmail.com>
2023-06-07 12:10:34 +0100

    Cyclic da fdp

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getUser,register,login,getUsers}=require('..controllers/users.controller')
+const {getUser,register,login,getUsers}=require('../controllers/users.controller')
 const {auth_user,auth_admin,auth_same_user} = require('../controllers/auth.controller') // Authentication Middleware 
 const {editUser,deleteUser,titles,badges} = require('../controllers/users.controller.js')
-const User = require('../Server/models/user.model')
+const User = require('../models/user.model')
 
 
 router.route('/')
